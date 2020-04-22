9c9966d9b947b3dd0cc3701eb8b26e3ed8eb80d2
Brandon Aldrich <brandon@aldr.io>
2020-04-22 17:07:33 -0400

    skeleton

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+import Game from "../Game";
+
+export default function App() {
+  return (
+    <div className="app">
+      This is the app...
+      <Game />
+    </div>
+  );
+}
---
+++ src/Game/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+export default function Game() {
+  return <div className="game">This is the game...</div>;
+}
